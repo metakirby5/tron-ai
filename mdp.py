5ed9145e60bc915eb0a9a8c67e2795242030a6fb
Richard Lin <rgl001@ucsd.edu>
2016-06-07 15:11:20 -0700

    Some mdp code, but policies are off

--- mdp.py
+++ mdp.py
@@ -21,9 +21,10 @@ config = {
         [1, 0],
         [-1, 0]
     ],
-    'max_iterations': 100,
-    'reward_pit': -10.0,
-    'reward_goal': 10.0,
+    'max_iterations': 200,
+    'discount_factor': 0.99,
+    'reward_pit': -0.05, #-10.0,
+    'reward_goal': 100.0,
     'reward_step': -0.05
 }
 
@@ -34,11 +35,29 @@ dirs = {
     'right':    lambda (dx, dy): ( dy, -dx),
 }
 
-types = {
+to_str = {
     tron.FLOOR: 'FLOOR',
     tron.WALL:  'WALL',
     tron.ME:    'ME',
-    tron.THEM:  'THEM'
+    tron.THEM:  'THEM',
+    tron.EAST:  '>',
+    tron.WEST:  '<',
+    tron.SOUTH: 'v',
+    tron.NORTH: '^'
+}
+
+rewards = {
+    tron.FLOOR: config['reward_step'],
+    tron.WALL:  config['reward_pit'],
+    tron.ME:    config['reward_step'],
+    tron.THEM:  config['reward_goal']
+}
+
+policies = {
+    ( 0,  1):  tron.EAST,
+    ( 0, -1):  tron.WEST,
+    ( 1,  0):  tron.SOUTH,
+    (-1,  0):  tron.NORTH,
 }
 
 class Graph(object):
@@ -49,7 +68,7 @@ class Graph(object):
         return '\n'.join(
             ''.join(r) for r in
             np.vectorize(
-                lambda n: n.policy or n.type
+                lambda n: to_str.get(n.policy, None) or n.type
             )(self.nodes)
         )
 
@@ -68,7 +87,7 @@ class Node(object):
 
     def __repr__(self):
         return '{}({}, {}) @ ({}, {})'.format(
-            types[self.type], self.policy, self.value, self.x, self.y)
+            to_str[self.type], self.policy, self.value, self.x, self.y)
 
     def add_neighbor(self, direction, neighbor):
         self.neighbors[direction] = neighbor
@@ -87,10 +106,10 @@ def build_graph(board, nodes):
 
             if 0 <= x < board.width and 0 <= y < board.height:
                 neighbor = nodes[x, y]
-                node.add_neighbor((dx, dy), neighbor)
+                if neighbor.type != tron.WALL:
+                    node.add_neighbor((dx, dy), neighbor)
 
     return Graph(nodes)
-
 
 def which_move(board):
 
@@ -101,11 +120,48 @@ def which_move(board):
 
     graph = build_graph(board, nodes)
 
+
+    #for src in graph.nodes.flat:
+    #    print >>stderr, "node:", types[src.type], ", neigh:", src.neighbors
+
+    # Map has updated, so recalculate optimal policy
+    for i in xrange(config['max_iterations']):
+
+        # For every node, calculate policies and values
+        delta = 0
+        for src in graph.nodes.flat:
+
+            # Find the best policy and value out of all actions
+            policy = None
+            best = src.value
+            for a in src.neighbors:
+
+                # Get the expected value over all transitions
+                value = 0
+                for dir, t in dirs.iteritems():
+                    try:
+                        dest = src.neighbors[t(a)]
+                    except KeyError:
+                        continue
+                    # reward = rewards['WALL' if src == dest else dest.type]
+                    reward = rewards[dest.type]
+                    value += 0.25 * (reward + config['discount_factor'] * dest.value)
+                    # value += config['prob_move_' + dir] * \
+                    #        (reward + config['discount_factor'] * dest.value)
+
+                # Update if we have a better expected value
+                if policy is None or value > best:
+                    policy = a
+                    best = value
+
+            delta += abs(best - src.value)
+            src.policy = policies.get(policy, policy)
+            src.value = best
+
+    move = graph.nodes[board.me()].policy
     print >>stderr, "GRAPH: <<\n", graph, "\n>>"
-
-    for src in graph.nodes.flat:
-        print >>stderr, "node:", types[src.type], ", neigh:", src.neighbors
-
+    print >>stderr, "moving:", to_str[move]
+    return move
 
     # fill in your code here. it must return one of the following directions:
     #   tron.NORTH, tron.EAST, tron.SOUTH, tron.WEST
@@ -113,7 +169,7 @@ def which_move(board):
     # For now, choose a legal move randomly.
     # Note that board.moves will produce [NORTH] if there are no
     # legal moves available.
-    return random.choice(board.moves())
+    # return random.choice(board.moves())
 
 # you do not need to modify this part
 for board in tron.Board.generate():
