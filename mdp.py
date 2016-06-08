a2ae05371623032a0cd93b37853b1cdc0c4976cb
Richard Lin <rgl001@ucsd.edu>
2016-06-08 01:03:09 -0700

    Working mdp, now need to make smarter

--- mdp.py
+++ mdp.py
@@ -23,16 +23,9 @@ config = {
     ],
     'max_iterations': 200,
     'discount_factor': 0.99,
-    'reward_pit': -0.05, #-10.0,
-    'reward_goal': 100.0,
+    'reward_pit': -10.0,
+    'reward_goal': 10.0,
     'reward_step': -0.05
-}
-
-dirs = {
-    'forward':  lambda (dx, dy): ( dx,  dy),
-    'backward': lambda (dx, dy): (-dx, -dy),
-    'left':     lambda (dx, dy): (-dy,  dx),
-    'right':    lambda (dx, dy): ( dy, -dx),
 }
 
 to_str = {
@@ -68,7 +61,8 @@ class Graph(object):
         return '\n'.join(
             ''.join(r) for r in
             np.vectorize(
-                lambda n: to_str.get(n.policy, None) or n.type
+                lambda n: to_str.get(n.policy, None) or n.kind
+                #lambda n: str(n.value)[0:5] + '\t'
             )(self.nodes)
         )
 
@@ -77,8 +71,8 @@ class Graph(object):
 
 
 class Node(object):
-    def __init__(self, type, x, y):
-        self.type = type
+    def __init__(self, kind, x, y):
+        self.kind = kind
         self.policy = None
         self.x = x
         self.y = y
@@ -87,7 +81,7 @@ class Node(object):
 
     def __repr__(self):
         return '{}({}, {}) @ ({}, {})'.format(
-            to_str[self.type], self.policy, self.value, self.x, self.y)
+            to_str[self.kind], self.policy, self.value, self.x, self.y)
 
     def add_neighbor(self, direction, neighbor):
         self.neighbors[direction] = neighbor
@@ -97,7 +91,8 @@ def build_graph(board, nodes):
 
     for node in nodes.flat:
 
-        if node.type == tron.WALL:
+        if node.kind == tron.WALL:
+            node.value = config['reward_pit']
             continue
 
         for dx, dy in config['move_list']:
@@ -105,9 +100,8 @@ def build_graph(board, nodes):
             y = node.y + dy
 
             if 0 <= x < board.width and 0 <= y < board.height:
-                neighbor = nodes[x, y]
-                if neighbor.type != tron.WALL:
-                    node.add_neighbor((dx, dy), neighbor)
+                neighbor = nodes[y, x]
+                node.add_neighbor((dy, dx), neighbor)
 
     return Graph(nodes)
 
@@ -120,15 +114,10 @@ def which_move(board):
 
     graph = build_graph(board, nodes)
 
-
-    #for src in graph.nodes.flat:
-    #    print >>stderr, "node:", types[src.type], ", neigh:", src.neighbors
-
     # Map has updated, so recalculate optimal policy
     for i in xrange(config['max_iterations']):
 
         # For every node, calculate policies and values
-        delta = 0
         for src in graph.nodes.flat:
 
             # Find the best policy and value out of all actions
@@ -136,27 +125,21 @@ def which_move(board):
             best = src.value
             for a in src.neighbors:
 
-                # Get the expected value over all transitions
-                value = 0
-                for dir, t in dirs.iteritems():
-                    try:
-                        dest = src.neighbors[t(a)]
-                    except KeyError:
-                        continue
-                    # reward = rewards['WALL' if src == dest else dest.type]
-                    reward = rewards[dest.type]
-                    value += 0.25 * (reward + config['discount_factor'] * dest.value)
-                    # value += config['prob_move_' + dir] * \
-                    #        (reward + config['discount_factor'] * dest.value)
+                # Get the value of taking this action (assume certain movement)
+                dest = src.neighbors[a]
+                reward = rewards[dest.kind]
+                value = (reward + config['discount_factor'] * dest.value)
 
                 # Update if we have a better expected value
                 if policy is None or value > best:
                     policy = a
                     best = value
 
-            delta += abs(best - src.value)
             src.policy = policies.get(policy, policy)
             src.value = best
+
+    for d, n in graph.nodes[board.me()].neighbors.iteritems():
+        print >>stderr, "\tdir=", d, ", val=", n.value, n.kind
 
     move = graph.nodes[board.me()].policy
     print >>stderr, "GRAPH: <<\n", graph, "\n>>"
